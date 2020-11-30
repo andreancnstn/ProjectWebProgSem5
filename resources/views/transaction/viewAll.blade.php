2f59c3e6131b9de7586124bebfe9db6bfdbbc004
andre <amzingdudu8@gmail.com>
2020-11-30 21:29:05 +0700

    add incrementing number in view all user transac

--- resources/views/transaction/viewAll.blade.php
+++ resources/views/transaction/viewAll.blade.php
@@ -6,12 +6,18 @@ Phizza Hut | All User Transaction
 
 @section('content')
     <div class="container">
+        @php
+        $i = 1;
+        @endphp
         @foreach ($data as $transac)
         <a href="{{ route('view_transac_detail', $transac->created_at) }}">
             <div class="row">
                 <div class="card m-2 col-12">
                     <div class="card-body" style="color: black">
-                        {{$transac->id}}
+                        {{$i}}
+                        @php
+                            $i++;
+                        @endphp
                         <label class="control-label">Transaction at {{ $transac->created_at }}</label><br>
                         <label class="control-label">User ID : {{ $transac->user_id }}</label><br>
                         <label class="control-label">Username : {{ $users->where('id', $transac->user_id)->first()->name }}</label>
@@ -19,6 +25,7 @@ Phizza Hut | All User Transaction
                 </div>
             </div>
         </a>
+
         @endforeach
     </div>
 @endsection
